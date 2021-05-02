4af5a0af0e02b1e6f60123be4cbb9b4fbe04e9ec
kelseyleewerner <kelsey.lee.werner@gmail.com>
2021-05-02 16:22:03 -0700

    refactor minimum sum to remove unecessary buffer of 0s

--- minimum_sum_descent.c
+++ minimum_sum_descent.c
@@ -9,50 +9,45 @@ int tabular_solution(int);
 
 #define HEIGHT 4
 
-int triangle[HEIGHT + 1][HEIGHT + 1] = {{0, 0, 0, 0, 0},
-                                        {0, 8, 6, 9, 6},
-                                        {0, 1, 4, 7, 0},
-                                        {0, 5, 4, 0, 0},
-                                        {0, 2, 0, 0, 0}};
+int triangle[HEIGHT][HEIGHT] = {{8, 6, 9, 6},
+                                {1, 4, 7, 0},
+                                {5, 4, 0, 0},
+                                {2, 0, 0, 0}};
 
-//int triangle[HEIGHT + 1][HEIGHT + 1] = {{0, 0, 0, 0, 0},
-  //                                      {0, 4, 1, 8, 3},
-    //                                    {0, 6, 5, 7, 0},
-      //                                  {0, 3, 4, 0, 0},
-        //                                {0, 2, 0, 0, 0}};
+//int triangle[HEIGHT][HEIGHT] = {{4, 1, 8, 3},
+    //                            {6, 5, 7, 0},
+      //                          {3, 4, 0, 0},
+        //                        {2, 0, 0, 0}};
 
-//int triangle[HEIGHT + 1][HEIGHT + 1] = {{0, 0, 0, 0, 0, 0},
-  //                                      {0, 1, 1, 1, 1, 1},
-    //                                    {0, 8, 6, 9, 6, 0},
-      //                                  {0, 1, 4, 7, 0, 0},
-        //                                {0, 5, 4, 0, 0, 0},
-          //                              {0, 2, 0, 0, 0, 0}};
+//int triangle[HEIGHT][HEIGHT] = {{1, 1, 1, 1, 1},
+    //                            {8, 6, 9, 6, 0},
+      //                          {1, 4, 7, 0, 0},
+        //                        {5, 4, 0, 0, 0},
+          //                      {2, 0, 0, 0, 0}};
 
-//int triangle[HEIGHT + 1][HEIGHT + 1] = {{0, 0},
-  //                                      {0, 2}};
+//int triangle[HEIGHT][HEIGHT] = {{2}};
 
-int mem_table[HEIGHT + 1][HEIGHT + 1];
-int tab_table[HEIGHT + 1][HEIGHT + 1];
+int mem_table[HEIGHT][HEIGHT];
+int tab_table[HEIGHT][HEIGHT];
 
 
 int main() {
 
-  printf("Basic Recursion: The minimum sum after descending the triangle is %d\n", basic_recursion(HEIGHT, 1));
+  printf("Basic Recursion: The minimum sum after descending the triangle is %d\n", basic_recursion(HEIGHT - 1, 0));
 
-  for (int i = 0; i <= HEIGHT; ++i) {
-    for (int j = 0; j <= HEIGHT; ++j) {
+  for (int i = 0; i < HEIGHT; ++i) {
+    for (int j = 0; j < HEIGHT; ++j) {
       mem_table[i][j] = 0;
     }
   }
 
-  printf("Memoized Solution: The minimum sum after descending the triangle is %d\n", memoized_solution(HEIGHT, 1));
+  printf("Memoized Solution: The minimum sum after descending the triangle is %d\n", memoized_solution(HEIGHT - 1, 0));
 
-  for (int i = 0; i <=HEIGHT; ++i) {
-    tab_table[0][i] = 0;
-    tab_table[1][i] = triangle[1][i];
+  for (int i = 0; i < HEIGHT; ++i) {
+    tab_table[0][i] = triangle[0][i];
   }
 
-  printf("Tabular Solution: The minimum sum after descending the triangle is %d\n", tabular_solution(HEIGHT));
+  printf("Tabular Solution: The minimum sum after descending the triangle is %d\n", tabular_solution(HEIGHT - 1));
 
 }
 
@@ -61,7 +56,7 @@ int basic_recursion(int row, int column)
 {
   int a, b, min;
 
-  if (row == 0) { return 0; }
+  if (row < 0) { return 0; }
 
   a = basic_recursion(row - 1, column);
   b = basic_recursion(row - 1, column + 1);
@@ -75,7 +70,7 @@ int memoized_solution(int row, int column)
 {
   int a, b, min;
 
-  if (row == 0) { return 0; }
+  if (row < 0) { return 0; }
 
   if (mem_table[row][column] > 0) { return mem_table[row][column]; }
 
@@ -94,10 +89,10 @@ int tabular_solution(int height)
   int a, b, min;
   int column = height - 1;
 
-  if (height == 1) { return tab_table[1][1]; }
+  if (height == 0) { return tab_table[0][0]; }
 
-  for (int r = 2; r <= height; ++r, --column) {
-    for (int c = 1; c <= column; ++c) {
+  for (int r = 1; r <= height; ++r, --column) {
+    for (int c = 0; c <= column; ++c) {
       a = tab_table[r - 1][c];
       b = tab_table[r - 1][c + 1];
       min = a < b ? a : b;
@@ -106,7 +101,7 @@ int tabular_solution(int height)
     }
   }
 
-  return tab_table[height][1];
+  return tab_table[height][0];
 }
 
 
