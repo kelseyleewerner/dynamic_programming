a27306e8b891943a1ec694560a57746ea5f6ba81
kelseyleewerner <kelsey.lee.werner@gmail.com>
2021-05-03 22:22:00 -0700

    memoized solution to rod cutting problem

--- rod_cutting.c
+++ rod_cutting.c
@@ -5,6 +5,7 @@
 
 
 int basic_recursion(int);
+int memoized_solution(int);
 
 #define LENGTH 8
 
@@ -12,13 +13,18 @@ int prices[LENGTH] = {1, 5, 8, 9, 10, 17, 17, 20};
 // int prices[LENGTH] = {3, 5, 8, 9, 10, 17, 17, 20};
 // int prices[LENGTH] = {3};
 
+int mem_table[LENGTH + 1];
+
 
 int main()
 {
 
   printf("Basic Recursion: The max sale price of a rod of %d unit(s) is: %d\n", LENGTH, basic_recursion(LENGTH));
 
+  mem_table[0] = 0;
+  for (int i = 1; i <= LENGTH; ++i) { mem_table[i] = -1; }
 
+  printf("Memoized Solution: The max sale price of a rod of %d unit(s) is: %d\n", LENGTH, memoized_solution(LENGTH));
 
 
 
@@ -41,6 +47,33 @@ int basic_recursion(int unit_length)
 
   return max;
 }
+
+
+int memoized_solution(int unit_length)
+{
+  int a, b;
+  int max = INT_MIN;
+
+  if (mem_table[unit_length] >= 0) { return mem_table[unit_length]; }
+
+  for (int i = 0; i < unit_length; ++i) {
+    a = max;
+    b = prices[i] + basic_recursion(unit_length - 1 - i);
+    max = a > b ? a : b;
+  }
+
+  mem_table[unit_length] = max;
+
+  return mem_table[unit_length];
+}
+
+
+
+
+
+
+
+
 
 
 
